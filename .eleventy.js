5ae3df39368f4d82e37646272fc59a4809717c3e
tomrsvr <4099754+tomrsvr@users.noreply.github.com>
2023-01-31 11:22:25 +0900

    HP Updates

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,12 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/app.js');
+
+    // Watch CSS files for changes
+    eleventyConfig.setBrowserSyncConfig({
+      files: './public/css/**/*.css'
+    });
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
       return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);

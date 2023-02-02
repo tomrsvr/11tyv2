f7acdf59388c79faa11a5ff4685de1c5c7a69fca
tomrsvr <4099754+tomrsvr@users.noreply.github.com>
2023-02-02 14:36:45 +0900

    HP Postcard V1

--- src/app.js
+++ src/app.js
@@ -186,3 +186,33 @@ anime.timeline({loop: false})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+
+
+  let constrain = 100;
+  let mouseOverContainer = document.getElementById("hero");
+  let postCard = document.getElementById("postcard");
+  
+  function transforms(x, y, el) {
+    let box = el.getBoundingClientRect();
+    let calcX = -(y - box.y - (box.height / 2)) / constrain;
+    let calcY = (x - box.x - (box.width / 2)) / constrain;
+    
+    return "perspective(900px) "
+      + "   rotateX("+ calcX +"deg) "
+      + "   rotateY("+ calcY +"deg) ";
+  };
+  
+   function transformElement(el, xyEl) {
+    el.style.transform  = transforms.apply(null, xyEl);
+  }
+  
+  mouseOverContainer.onmousemove = function(e) {
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([postCard]);
+  
+    window.requestAnimationFrame(function(){
+      transformElement(postCard, position);
+    });
+  };
+  
